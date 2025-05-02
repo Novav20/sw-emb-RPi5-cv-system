f296b813d7133cce0b20f71d3018a4e4eeff5c79
Novav20 <illus.kun@gmail.com>
2025-05-02 16:28:33 -0500

    Add placeholder for main CV script

---
+++ main_cv.py
@@ -0,0 +1,20 @@
+import time
+
+print("CV Application - RPi5")
+print("Initializing...")
+
+# --- Configuration ---
+# TODO: Load model, define constants
+
+# --- Main Loop ---
+# TODO: Set up Flask/FastAPI endpoint
+# TODO: Receive image, run inference, send command
+
+print("Setup placeholder complete. Waiting for connections...")
+
+# Keep running for now (replace with web server later)
+# try:
+#    while True:
+#        time.sleep(10)
+# except KeyboardInterrupt:
+#    print("Exiting.")
